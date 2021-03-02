c1e61e4d8d829409d962b117955b5485358e678a
Winston Majano <winstonmajano@gmail.com>
2021-03-02 08:59:01 +0100

    ejemplo de directiva  v-for

---
+++ 04-V-For/main.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            user: {
+                name: "Winston Majano",
+                age: 23,
+                title: "Curso de vue3",
+                muvies: ["El grinch", "Rapido y furioso", "El dia final", "Superman", "Spiderman"]
+            
+            }
+            
+        }
+    }
+});
+
